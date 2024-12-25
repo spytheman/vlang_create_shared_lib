88187593d0e5efd0e84a7a31e967622e1c5201da
Delyan Angelov <delian66@gmail.com>
2024-12-25 09:31:30 +0200

    use dlsym too

--- t.c
+++ t.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <dlfcn.h>
+#include <stdint.h>
+
 
 int main(){
    const char *library_path = "/Users/delyanangelov/code/misc/2024_12_25__09/check.dylib";
@@ -9,5 +11,17 @@ int main(){
       return 1;
    }
    printf("INFO: %s OK\n", library_path);
+   uint64_t (*add_fn)(uint64_t, uint64_t) = dlsym(lib, "check__add");
+   if(add_fn == NULL){
+      fprintf(stderr, "ERROR: check_add could not be found in shared library. error: %s\n", dlerror());
+      return 1;
+   }
+   uint64_t a,b,c;
+   a = 123;
+   b = 456;
+   c = add_fn(a,b);
+   printf("     a: %llu\n", a);
+   printf("     b: %llu\n", b);
+   printf("     c: %llu\n", c);
    return 0;
 }
